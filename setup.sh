0ab422e73b141b718931c8d3583c9c7f0d421291
Kieren Funk <kierenfunk@gmail.com>
2022-12-01 19:17:56 +0100

    Attempt to see if setup script can see github env variables

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
+echo "${{ env.REPOSITORY_NAME }}"
 
-
-cp setup.cfg temp.cfg
-sed -i -e 's/python_template/new_repo/g' temp.cfg
-sed -i -e 's/python-template/new-repo/g' temp.cfg
-cp -r python_template new_repo
+# cp setup.cfg temp.cfg
+# sed -i -e 's/python_template/new_repo/g' temp.cfg
+# sed -i -e 's/python-template/new-repo/g' temp.cfg
+# cp -r python_template new_repo
