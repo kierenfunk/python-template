96398f9e36c600401d778c6774f355679850c630
Kieren Funk <kierenfunk@gmail.com>
2022-12-01 20:03:51 +0100

    update init.sh to modify README.md

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@
 
 sed -i -e "s/python_template/$1/g" setup.cfg
 sed -i -e "s/python-template/$2/g" setup.cfg
+sed -i -e "s/python_template/$1/g" README.md
+sed -i -e "s/python-template/$2/g" README.md
 mv python_template "$1"
 sed -i -e "s/python_template/$1/g" tests/test_main.py
 sed -i -e "s/python_template/$1/g" Pipfile
